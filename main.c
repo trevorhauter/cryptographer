a85d0ae2ec9dd24ed2b066a22123894b87d7f4ea
Trevor <trevor@hauter.dev>
2025-03-23 19:16:53 -0400

    Allow for one way alphabet soup encryption

--- main.c
+++ main.c
@@ -1,13 +1,31 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
+// Very simple encryption func
+void alphabet_soup(char *message) {
+  int i;
+  int asciiValue;
+
+  for (i = 0; i < strlen(message); i++) {
+    // Get the ascii value of the letter - increment one
+    asciiValue = (int)message[i] + 1;
+    // This converts it back to a lettr
+    message[i] = '0' + asciiValue;
+  }
+}
+
+// Main entry point to cryptographer
 int main() {
   int phraseLength = 144;
   char phrase[phraseLength];
+  char encrypted_phrase[phraseLength];
+
   printf("Enter a phrase to be encrypted (max length: ");
   printf("%d", phraseLength);
   printf("):");
 
   fgets(phrase, phraseLength, stdin);
+  alphabet_soup(phrase);
   printf("%s", phrase);
 }
